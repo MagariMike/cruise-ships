3f4da62fcaea0307e41c7d721db6656ed55bbc6d
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-26 19:05:49 +0000

    initial attribute tests complete

--- __tests__/cruise-ships.test.js
+++ __tests__/cruise-ships.test.js
@@ -6,5 +6,13 @@ describe('constructor',() => {
         
     })
 
-    
-})
+    it('returns passenger count', () => {
+        const cruiseShip1 = new CruiseShip('Bahama Cruise', 9);
+        expect(cruiseShip1.passengers).toEqual(9);
+    })
+
+    it('returns startingPoint', () => { 
+        const cruiseShip2 = new CruiseShip('Grimsby Cruise', 5, 'Grimsby');
+        expect(cruiseShip2.startingPoint).toEqual('Grimsby');
+    })
+}) 
