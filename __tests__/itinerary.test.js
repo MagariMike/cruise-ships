8be297f7e7918634e64ed1b63bcc6836a77a57b1
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-01-14 13:34:50 +0000

    refactoring Ship setSail method to incorporate previousPort property

--- __tests__/itinerary.test.js
+++ __tests__/itinerary.test.js
@@ -1,8 +1,14 @@
 const Itinerary = require('../src/itinerary');
+const Port = require('../src/port');
 
 describe('Itinerary', () => { 
     it('Can be instantiated', () => { 
         expect(new Itinerary()).toBeInstanceOf(Object);
+    })
+
+    it('has a ports property', () => {
+        const testItinerary = new Itinerary('test');
+        expect(testItinerary.ports).toEqual('test');
     })
 })
 
--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,31 +1,43 @@
-const CruiseShip = require('../src/ship');
+const Ship = require('../src/ship');
 const Port = require('../src/port');
 
 
 describe('Ship',() => { 
     it('Can be instantiated', () => {
-        expect(new CruiseShip()).toBeInstanceOf(Object);
+        expect(new Ship()).toBeInstanceOf(Object);
         
     })
     
     
     it('returns startingPort', () => { 
-        const cruiseShip2 = new CruiseShip('Grimsby Cruise', Port);
-        expect(cruiseShip2.currentPort).toBe(Port);
+        const currentPort = new Port('Brighton');
+        // currentPort = { name: Brighton }
+        const cruiseShip2 = new Ship('Brighton');
+        expect(cruiseShip2.currentPort).toEqual('Brighton');
     })
 
     describe('setSail', () => {
-        it('', () => { 
-            let cruiseShip3 = new CruiseShip('Dave', Port); 
+        const testPort1 = new Port('Chester')
+        const cruiseShip3 = new Ship(testPort1);
+        
+        it('sets current port as null', () => { 
+             
             cruiseShip3.setSail();
-            expect(cruiseShip3.setSail()).toBeFalsy();
+            expect(cruiseShip3.currentPort).toBeFalsy();
+            
         })
+        
+        it('sets previous port to current port', () => { 
+            expect(cruiseShip3.previousPort).toEqual(testPort1);
+            // research testing for an object value - most likely .toMatch instead of .toEqual
+        });
+        
     })
 
     describe('dock', () => {
         it('Can dock at a different port' , () => {
             const dover = new Port('dover');
-            const ship = new CruiseShip('ship', Port);
+            const ship = new Ship('ship', Port);
 
             const calais = new Port('Calais');
             ship.dock(calais);
