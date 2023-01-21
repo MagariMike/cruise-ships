a0a81ebfb62d992891ae933c1af5640cb60e5ccc
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-01-21 14:28:19 +0000

    addShip test and method written and passing

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,6 +1,7 @@
 const CruiseShip = require('../src/ship');
 const Port = require('../src/port');
 const Itinerary = require('../src/itinerary');
+const Ship = require('../src/ship');
 
 describe('Port', () => {
     it('Can be instantiated', () => { 
@@ -10,5 +11,13 @@ describe('Port', () => {
     it('returns name', () => { 
         const port1 = new Port('Grimsby');
         expect(port1.name).toEqual('Grimsby');
+    })
+
+    it('Can add a ship', () => {
+        const port = new Port('dover', []);
+        const ship = {};
+        port.addShip(ship);
+        expect(port.ships).toEqual([ship])
+    
     })
 })
--- src/port.js
+++ src/port.js
@@ -1,17 +1,19 @@
 class Port {
-    constructor(name){
+    constructor(name,ships){
         this.name = name
-    
+        this.ships = ships
     }
 
 
-}
+
+
+addShip(ship) { 
+    this.ships.push(ship);  
+};
 
 
 
-
-
-
+};
 
 
 
